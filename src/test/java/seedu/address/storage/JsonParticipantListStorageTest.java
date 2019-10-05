22c9795fc578ac501ec606f57bf5f9e5bec8acee
brianyenna <brianyen.na@gmail.com>
2019-10-05 18:01:36 +0800

    Stop-gap fix for compileTestJava

---
+++ src/test/java/seedu/address/storage/JsonParticipantListStorageTest.java
@@ -0,0 +1,26 @@
+package seedu.address.storage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JsonParticipantListStorageTest {
+
+    @org.junit.jupiter.api.Test
+    void getParticipantListFilePath() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void readParticipantList() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testReadParticipantList() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void saveParticipantList() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testSaveParticipantList() {
+    }
+}
